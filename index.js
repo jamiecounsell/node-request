f05f4fa38c048f6e6ea28d88fd11e3659c5d4491
Jamie Counsell <jamiecounsell@me.com>
2017-10-24 11:31:41 -0400

    complete assignment, load JSON results from Chuck Norris joke API

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const request = require('request');
 
-// no answers here - the answer is within you!
+request.get("http://api.icndb.com/jokes/random", {
+    'exclude': ['explicit']
+}, (error, response, body) => {
+    if (error) {
+        console.error(error);
+    } else if (response.statusCode !== 200) {
+        console.error(body);
+    } else {
+        let data = JSON.parse(body);
+        console.log(unescape(data['value']['joke']));
+    }
+});
